5099a2da52552d5c19fc557c12b5a9fd11f9c3f2
HumanEntity <human_entity@icloud.com>
2023-12-22 21:30:00 +0100

    Git plugins

---
+++ lua/plugins/git.lua
@@ -0,0 +1,79 @@
+return {
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			local gitsigns = pcall(require, "gitsigns")
+			if not status_ok then
+				return
+			end
+
+			gitsigns.setup({
+				signs = {
+					add = { hl = "GitSignsAdd", text = "+", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
+					change = {
+						hl = "GitSignsChange",
+						text = "~",
+						numhl = "GitSignsChangeNr",
+						linehl = "GitSignsChangeLn",
+					},
+					delete = {
+						hl = "GitSignsDelete",
+						text = "-",
+						numhl = "GitSignsDeleteNr",
+						linehl = "GitSignsDeleteLn",
+					},
+					topdelete = {
+						hl = "GitSignsDelete",
+						text = "-",
+						numhl = "GitSignsDeleteNr",
+						linehl = "GitSignsDeleteLn",
+					},
+					changedelete = {
+						hl = "GitSignsChange",
+						text = "-",
+						numhl = "GitSignsChangeNr",
+						linehl = "GitSignsChangeLn",
+					},
+				},
+				signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
+				numhl = true, -- Toggle with `:Gitsigns toggle_numhl`
+				linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
+				word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
+				current_line_blame = true,
+				current_line_blame_opts = {
+					virt_text = true,
+					virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+					delay = 1000,
+					ignore_whitespace = false,
+				},
+				current_line_blame_formatter_opts = {
+					relative_time = false,
+				},
+			})
+		end,
+	},
+	{ "tpope/vim-fugitive", event = "VeryLazy" },
+	{ "rhysd/git-messenger.vim" },
+	{
+		"ThePrimeagen/git-worktree.nvim",
+		dependencies = { "nvim-telescope/telescope.nvim" },
+		config = function()
+			require("git-worktree").setup({})
+			require("telescope").load_extension("git_worktree")
+		end,
+	},
+	{
+		"NeogitOrg/neogit",
+		dependencies = {
+			"nvim-lua/plenary.nvim", -- required
+			"nvim-telescope/telescope.nvim", -- optional
+			"sindrets/diffview.nvim", -- optional
+			"ibhagwan/fzf-lua", -- optional
+		},
+		event = "VeryLazy",
+		config = function()
+			local neogit = require("neogit")
+			neogit.setup()
+		end,
+	},
+}
