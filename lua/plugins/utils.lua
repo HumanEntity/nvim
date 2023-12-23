d03fd95b0ba2e50bfb86ae2a3a5c84fc57411a31
HumanEntity <human_entity@icloud.com>
2023-12-23 21:43:02 +0100

    Added undotree

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -51,4 +51,14 @@ return {
 		end,
 		lazy = false,
 	},
+	{
+		"jiaoshijie/undotree",
+		dependencies = "nvim-lua/plenary.nvim",
+		config = function()
+			require("undotree").setup()
+		end,
+		keys = { -- load the plugin only when using it's keybinding:
+			{ "<leader>u", "<cmd>lua require('undotree').toggle()<cr>" },
+		},
+	},
 }
