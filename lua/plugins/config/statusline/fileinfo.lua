1f5e77f83cf21195c845b48497fa86af4949063d
HumanEntity <human_entity@icloud.com>
2023-12-28 12:41:09 +0100

    Removed FileLastModified from fileinfo

--- lua/plugins/config/statusline/fileinfo.lua
+++ lua/plugins/config/statusline/fileinfo.lua
@@ -26,14 +26,6 @@ local FileSize = {
 	end,
 }
 
-local FileLastModified = {
-	-- did you know? Vim is full of functions!
-	provider = function()
-		local ftime = vim.fn.getftime(vim.api.nvim_buf_get_name(0))
-		return (ftime > 0) and os.date("%c", ftime)
-	end,
-}
-
 local FileInfo = {
 	-- FileType,
 	FileEncoding,
