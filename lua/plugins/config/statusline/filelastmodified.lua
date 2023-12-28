ed2b58563524984f7f30cdcd2b9133d61b106b66
HumanEntity <human_entity@icloud.com>
2023-12-28 14:09:55 +0100

    Added macrorec component

--- lua/plugins/config/statusline/filelastmodified.lua
+++ lua/plugins/config/statusline/filelastmodified.lua
@@ -2,10 +2,15 @@ local FileLastModified = {
 	-- did you know? Vim is full of functions!
 	flexible = 1,
 	{
-
 		provider = function()
 			local ftime = vim.fn.getftime(vim.api.nvim_buf_get_name(0))
 			return (ftime > 0) and os.date("%c", ftime)
+		end,
+	},
+	{
+		provider = function()
+			local ftime = vim.fn.getftime(vim.api.nvim_buf_get_name(0))
+			return (ftime > 0) and os.date("%y/%m/%d/%H:%M", ftime)
 		end,
 	},
 	{
