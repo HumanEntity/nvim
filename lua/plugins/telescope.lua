21d0f31b8bbe74e9d4725defcd0e1df18bda1c6d
HumanEntity <human_entity@icloud.com>
2024-02-08 10:59:57 +0100

    Remove telescope-ui-select

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,7 +6,6 @@ return {
 		dependencies = {
 			"nvim-lua/plenary.nvim",
 			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-			"nvim-telescope/telescope-ui-select.nvim",
 		},
 		config = function()
 			local actions = require("telescope.actions")
@@ -20,36 +19,15 @@ return {
 					},
 				},
 				extensions = {
-					-- file_browser = {
-					-- 	-- theme = "ivy",
-					-- 	-- disables netrw and use telescope-file-browser in its place
-					-- 	hijack_netrw = true,
-					-- 	mappings = {
-					-- 		["i"] = {
-					-- 			-- your custom insert mode mappings
-					-- 		},
-					-- 		["n"] = {
-					-- 			-- your custom normal mode mappings
-					-- 		},
-					-- 	},
-					-- },
 					fzf = {
 						fuzzy = true,
 						override_generic_sorter = true,
 						override_file_sorter = true,
 						case_mode = "smart_case",
 					},
-					["ui-select"] = {
-						require("telescope.themes").get_dropdown({
-							-- even more opts
-						}),
-					},
 				},
 			})
 
-			require("telescope").load_extension("ui-select")
-			-- To get fzf loaded and working with telescope, you need to call
-			-- load_extension, somewhere after setup function:
 			require("telescope").load_extension("fzf")
 			-- require("telescope").load_extension("hoogle")
 			-- require("telescope").load_extension("projects")
