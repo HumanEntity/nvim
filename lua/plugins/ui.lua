6fd038c66cdd3711e0c1ee1e862e4f898a88a42f
HumanEntity <human_entity@icloud.com>
2024-04-08 12:47:08 +0200

    Update to indent blankline

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -33,29 +33,40 @@ return {
 	},
 	{
 		"lukas-reineke/indent-blankline.nvim",
-		dependencies = "nvim-treesitter",
-		tag = "v3.4.2",
+		dependencies = "nvim-treesitter/nvim-treesitter",
+		-- tag = "v3.5.3",
 		main = "ibl",
-		opts = {
-			indent = { char = "▏" },
-			whitespace = { remove_blankline_trail = true },
-			exclude = {
-				filetypes = {
-					"help",
-					"terminal",
-					"packer",
-					"lspinfo",
-					"TelescopePrompt",
-					"TelescopeResults",
-					"alpha",
+		config = function()
+			require("ibl").setup({
+				indent = {
+					-- highlight = highlight,
+					char = "▏",
 				},
-				buftypes = { "terminal", "oil" },
-			},
+				whitespace = {
+					remove_blankline_trail = true,
+					-- highlight = {
+					-- 	"CursorColumn",
+					-- 	"Whitespace",
+					-- },
+				}, -- char = "·" },
+				exclude = {
+					filetypes = {
+						"help",
+						"terminal",
+						"packer",
+						"lspinfo",
+						"TelescopePrompt",
+						"TelescopeResults",
+						"alpha",
+					},
+					buftypes = { "terminal", "oil" },
+				},
+			})
 			-- show_trailing_blankline_indent = false,
 			-- show_first_indent_level = false,
 			-- space_char_blankline = " ",
 			-- use_treesitter = true,
-		},
+		end,
 	},
 	{
 		"goolord/alpha-nvim",
