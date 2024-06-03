f59eee003a9c7073356260b8a9bed9030d385eb3
HumanEntity <human_entity@icloud.com>
2024-06-03 21:49:39 +0200

    Todo comments

--- lua/he/lazy/utils.lua
+++ lua/he/lazy/utils.lua
@@ -11,4 +11,17 @@ return {
 			{ "gb", mode = "v" },
 		},
 	},
+	{
+		"folke/todo-comments.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = {
+			gui_style = {
+				fg = "NONE", -- The gui style to use for the fg highlight group.
+				bg = "BOLD", -- The gui style to use for the bg highlight group.
+			},
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+	},
 }
