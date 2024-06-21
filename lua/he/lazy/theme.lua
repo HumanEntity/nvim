ba1e8e3d773fa309e6f5d40286561804c5a0d93d
HumanEntity <human_entity@icloud.com>
2024-06-21 12:57:34 +0200

    feat(theme): update

--- lua/he/lazy/theme.lua
+++ lua/he/lazy/theme.lua
@@ -1,33 +1,33 @@
 local function setup_colors(theme)
-	vim.cmd.colorscheme(theme)
+    vim.cmd.colorscheme(theme)
 
-	local utils = require("he.utils")
+    local utils = require("he.utils")
 
-	local toclear = {
-		"Normal",
-		"NormalFloat",
-		"DiagnosticVirtualTextHint",
-		"DiagnosticVirtualTextOk",
-		"DiagnosticVirtualTextInfo",
-		"DiagnosticVirtualTextWarn",
-		"DiagnosticVirtualTextError",
-		"TelescopeNormal",
-		"TelescopeBorder",
-	}
+    local toclear = {
+        "Normal",
+        "NormalFloat",
+        "DiagnosticVirtualTextHint",
+        "DiagnosticVirtualTextOk",
+        "DiagnosticVirtualTextInfo",
+        "DiagnosticVirtualTextWarn",
+        "DiagnosticVirtualTextError",
+        "TelescopeNormal",
+        "TelescopeBorder",
+    }
 
-	utils:clear_group(toclear)
+    utils:clear_group(toclear)
 end
 
 return {
-	{
-		"craftzdog/solarized-osaka.nvim",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			require("solarized-osaka").setup({
-				transparent = true,
-			})
-			vim.cmd("colorscheme solarized-osaka")
-		end,
-	},
+    {
+        "craftzdog/solarized-osaka.nvim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            require("solarized-osaka").setup({
+                transparent = true,
+            })
+            setup_colors("solarized-osaka")
+        end,
+    },
 }
