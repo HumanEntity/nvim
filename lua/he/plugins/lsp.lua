a96c3d4c3b6c9767b2ab532ccda28f9f5ccf9f65
HumanEntity <human_entity@icloud.com>
2025-04-09 16:23:52 +0200

    Blink-cmp for completion

--- lua/he/plugins/lsp.lua
+++ lua/he/plugins/lsp.lua
@@ -1,248 +1,309 @@
 return {
-    {
-        "neovim/nvim-lspconfig",
-        event = { "BufReadPost", "BufWritePost", "BufNewFile" },
-        dependencies = {
-            "williamboman/mason.nvim",
-            "williamboman/mason-lspconfig.nvim",
-            "L3MON4D3/LuaSnip",
-            "j-hui/fidget.nvim",
-            "hrsh7th/cmp-nvim-lsp",
-            "windwp/nvim-autopairs",
-        },
-        config = function()
-            local cmp_lsp = require("cmp_nvim_lsp")
-            local capabilities = vim.tbl_deep_extend(
-                "force",
-                {},
-                vim.lsp.protocol.make_client_capabilities(),
-                cmp_lsp.default_capabilities()
-            )
-
-            local function border(hl_name)
-                return {
-                    { "╭", hl_name },
-                    { "─", hl_name },
-                    { "╮", hl_name },
-                    { "│", hl_name },
-                    { "╯", hl_name },
-                    { "─", hl_name },
-                    { "╰", hl_name },
-                    { "│", hl_name },
-                }
-            end
-
-            vim.lsp.handlers["textDocument/hover"] =
-                vim.lsp.with(vim.lsp.handlers.hover, { border = border("FloatBorder") })
-
-            local on_attach = function(client, bufnr) end
-
-            require("fidget").setup()
-            require("mason").setup()
-            require("mason-lspconfig").setup({
-                ensure_installed = {
-                    "lua_ls",
-                    "rust_analyzer",
-                    "taplo",
-                    "zls",
-                    "clangd",
-                },
-                handlers = {
-                    function(server_name) -- default handler (optional)
-                        require("lspconfig")[server_name].setup({
-                            capabilities = capabilities,
-                            on_attach = on_attach,
-                        })
-                    end,
-                    ["rust_analyzer"] = function()
-                        local lspconfig = require("lspconfig")
-                        lspconfig.rust_analyzer.setup({
-                            capabilities = capabilities,
-                            on_attach = on_attach,
-                            settings = {
-                                ["rust-analyzer"] = {
-                                    checkOnSave = {
-                                        command = "clippy",
-                                    },
-                                },
-                            },
-                        })
-                    end,
-                    ["lua_ls"] = function()
-                        local lspconfig = require("lspconfig")
-                        lspconfig.lua_ls.setup({
-                            capabilities = capabilities,
-                            on_attach = on_attach,
-                            settings = {
-                                Lua = {
-                                    diagnostics = {
-                                        globals = { "vim", "it", "describe", "before_each", "after_each" },
-                                    },
-                                    workspace = {
-                                        checkThirdParty = false,
-                                        telemetry = { enable = false },
-                                        library = {
-                                            "${3rd}/love2d/library",
-                                        },
-                                    },
-                                },
-                            },
-                        })
-                    end,
-                    ["arduino_language_server"] = function()
-                        local lspconfig = require("lspconfig")
-
-                        local mason_registry = require("mason-registry")
-
-                        local clangd = mason_registry.get_package("clangd")
-                        local clangd_exe = clangd:get_install_path() .. "/clangd_18.1.3/bin/clangd"
-
-                        lspconfig.arduino_language_server.setup({
-                            -- capabilities = capabilities,
-                            on_attach = on_attach,
-                            cmd = {
-                                "arduino-language-server",
-                                "-cli-config",
-                                "/Users/tomek/Library/Arduino15/arduino-cli.yaml",
-                                -- "-fqbn",
-                                -- "esp32:esp32:esp32s2",
-                                "-clangd",
-                                clangd_exe,
-                            },
-                        })
-                    end,
-                },
-            })
-            require("lspconfig").ols.setup({})
-
-            require("nvim-autopairs").setup({})
-
-            vim.diagnostic.config({
-                -- update_in_insert = true,
-                float = {
-                    focusable = false,
-                    style = "minimal",
-                    border = "rounded",
-                    source = "always",
-                    header = "",
-                    prefix = "",
-                },
-            })
-        end,
+  {
+    "neovim/nvim-lspconfig",
+    event = { "BufReadPost", "BufWritePost", "BufNewFile" },
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+      "L3MON4D3/LuaSnip",
+      "j-hui/fidget.nvim",
+      -- "hrsh7th/cmp-nvim-lsp",
+      "windwp/nvim-autopairs",
     },
-    {
-        "hrsh7th/nvim-cmp",
-        event = "InsertEnter",
-        dependencies = {
-            "hrsh7th/cmp-nvim-lsp",
-            "hrsh7th/cmp-buffer",
-            "hrsh7th/cmp-path",
-            "hrsh7th/cmp-cmdline",
-            "saadparwaiz1/cmp_luasnip",
+    config = function()
+      -- local cmp_lsp = require("cmp_nvim_lsp")
+      local capabilities = vim.tbl_deep_extend(
+        "force",
+        {},
+        vim.lsp.protocol.make_client_capabilities()
+      -- cmp_lsp.default_capabilities()
+      )
+      capabilities = require("blink.cmp").get_lsp_capabilities(capabilities)
+
+      local function border(hl_name)
+        return {
+          { "╭", hl_name },
+          { "─", hl_name },
+          { "╮", hl_name },
+          { "│", hl_name },
+          { "╯", hl_name },
+          { "─", hl_name },
+          { "╰", hl_name },
+          { "│", hl_name },
+        }
+      end
+
+      vim.lsp.handlers["textDocument/hover"] =
+          vim.lsp.with(vim.lsp.handlers.hover, { border = border("FloatBorder") })
+
+      local on_attach = function(client, bufnr) end
+
+      require("fidget").setup({})
+      require("mason").setup()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "rust_analyzer",
+          "taplo",
+          -- "zls",
+          --"clangd",
+          -- "harper_ls",
         },
-        opts = function()
-            local auto_select = true
-            local cmp = require("cmp")
-            local cmp_select = { behavior = cmp.SelectBehavior.Select }
-            return {
-                completion = {
-                    completeopt = "menu,menuone,noinsert" .. (auto_select and "" or ",noselect"),
+        handlers = {
+          function(server_name)           -- default handler (optional)
+            require("lspconfig")[server_name].setup({
+              capabilities = capabilities,
+              on_attach = on_attach,
+            })
+          end,
+          -- ["zls"] = function()
+          --   local lspconfig = require("lspconfig")
+          -- end,
+          ["rust_analyzer"] = function()
+            local lspconfig = require("lspconfig")
+            lspconfig.rust_analyzer.setup({
+              capabilities = capabilities,
+              on_attach = on_attach,
+              settings = {
+                ["rust-analyzer"] = {
+                  checkOnSave = {
+                    command = "clippy",
+                  },
                 },
-                preselect = auto_select and cmp.PreselectMode.Item or cmp.PreselectMode.None,
-                snippet = {
-                    expand = function(args)
-                        require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
-                    end,
-                },
-                formatting = {
-                    fields = {
-                        cmp.ItemField.Abbr,
-                        cmp.ItemField.Kind,
-                        cmp.ItemField.Menu,
+              },
+            })
+          end,
+          ["lua_ls"] = function()
+            local lspconfig = require("lspconfig")
+            lspconfig.lua_ls.setup({
+              capabilities = capabilities,
+              on_attach = on_attach,
+              settings = {
+                Lua = {
+                  diagnostics = {
+                    globals = { "vim", "it", "describe", "before_each", "after_each" },
+                  },
+                  workspace = {
+                    checkThirdParty = false,
+                    telemetry = { enable = false },
+                    library = {
+                      "${3rd}/love2d/library",
                     },
-                    format = function(entry, item)
-                        -- item.kind = string.sub(item.kind, 1, 1)
-
-                        local symbol_map = {
-                            Text = "󰦨",
-                            Method = "󰆧",
-                            Function = "󰊕",
-                            Constructor = "",
-                            Field = "",
-                            Variable = "󰫧",
-                            Class = "",
-                            Interface = "󱘎",
-                            Module = "󰆧",
-                            Property = "",
-                            Unit = "",
-                            Value = "󰀬",
-                            Enum = "",
-                            Keyword = "",
-                            Snippet = "",
-                            Color = "",
-                            File = "",
-                            Reference = "",
-                            Folder = "",
-                            EnumMember = "",
-                            Constant = "󰏿",
-                            Struct = "",
-                            Event = "",
-                            Operator = "",
-                            TypeParameter = "",
-                            Empty = "",
-                        }
-
-                        item.kind = symbol_map[item.kind or "Empty"] .. " " .. item.kind
-
-                        item.menu = ({
-                            buffer = "[Buffer]",
-                            nvim_lsp = "[LSP]",
-                            luasnip = "[LuaSnip]",
-                            nvim_lua = "[Lua]",
-                            latex_symbols = "[LaTeX]",
-                        })[entry.source.name]
-
-                        local widths = {
-                            abbr = vim.g.cmp_widths and vim.g.cmp_widths.abbr or 40,
-                            menu = vim.g.cmp_widths and vim.g.cmp_widths.menu or 30,
-                        }
-
-                        for key, width in pairs(widths) do
-                            if item[key] and vim.fn.strdisplaywidth(item[key]) > width then
-                                item[key] = vim.fn.strcharpart(item[key], 0, width - 1) .. "…"
-                            end
-                        end
-
-                        return item
-                    end,
+                  },
                 },
-                mapping = {
-                    ["<S-Tab>"] = cmp.mapping.select_next_item(cmp_select),
-                    ["<Tab>"] = cmp.mapping.confirm({ select = true, behavior = cmp.ConfirmBehavior.Replace }),
-                    ["<C-j>"] = cmp.mapping.select_next_item(cmp_select),
-                    ["<C-k>"] = cmp.mapping.select_prev_item(cmp_select),
-                    ["<C-y>"] = cmp.mapping.confirm({ select = true, behavior = cmp.ConfirmBehavior.Replace }),
-                    ["<C-h>"] = function()
-                        vim.lsp.buf.signature_help()
-                    end,
+              },
+            })
+          end,
+          ["arduino_language_server"] = function()
+            local lspconfig = require("lspconfig")
 
-                    ["<C-Space>"] = cmp.mapping.complete(),
-                },
-                sources = cmp.config.sources({
-                    { name = "path" },
-                    { name = "luasnip", keyword_length = 1 },
-                    { name = "nvim_lsp", keyword_length = 2 },
-                    { name = "buffer", keyword_length = 1 },
-                }),
-                window = {
-                    documentation = cmp.config.window.bordered(),
-                },
-                experimental = {
-                    ghost_text = {
-                        hl_group = "CmpGhostText",
-                    },
-                },
-            }
-        end,
+            local mason_registry = require("mason-registry")
+
+            local clangd = mason_registry.get_package("clangd")
+            local clangd_exe = clangd:get_install_path() .. "/clangd_18.1.3/bin/clangd"
+
+            lspconfig.arduino_language_server.setup({
+              -- capabilities = capabilities,
+              on_attach = on_attach,
+              cmd = {
+                "arduino-language-server",
+                "-cli-config",
+                "/Users/tomek/Library/Arduino15/arduino-cli.yaml",
+                -- "-fqbn",
+                -- "esp32:esp32:esp32s2",
+                "-clangd",
+                clangd_exe,
+              },
+            })
+          end,
+          ["clangd"] = function()
+            local lspconfig = require("lspconfig")
+
+            -- Setup commented out due to tests with ccls (Performed)
+
+            lspconfig.clangd.setup({
+              on_attach = on_attach,
+              capabilities = capabilities,
+              cmd = {
+                "clangd",
+                "--header-insertion=never",
+              },
+            })
+          end,
+        },
+      })
+      require("lspconfig").ols.setup({
+        cmd = { os.getenv("HOME") .. "/code/tools/ols/ols" },
+      })
+      require("lspconfig").ocamllsp.setup({
+        on_attach = on_attach,
+        capabilities = capabilities,
+      })
+      do
+        local lspconfig = require("lspconfig")
+
+        local mason_registry = require("mason-registry")
+
+        local clangd = mason_registry.get_package("clangd")
+        local clangd_exe = clangd:get_install_path() .. "/clangd_18.1.3/bin/clangd"
+
+        lspconfig.arduino_language_server.setup({
+          -- capabilities = capabilities,
+          on_attach = on_attach,
+          cmd = {
+            "arduino-language-server",
+            "-cli-config",
+            "/Users/tomek/Library/Arduino15/arduino-cli.yaml",
+            -- "-fqbn",
+            -- "esp32:esp32:esp32s2",
+            "-clangd",
+            clangd_exe,
+          },
+        })
+      end
+      -- require("lspconfig").zls.setup({
+      --   root_dir = require("lspconfig").util.root_pattern(".git", "build.zig", "zls.json"),
+      --   settings = {
+      --     zls = {
+      --       enable_inlay_hints = true,
+      --       enable_snippets = true,
+      --       warn_style = true,
+      --     },
+      --   },
+      -- })
+      -- vim.g.zig_fmt_parse_errors = 0
+      -- vim.g.zig_fmt_autosave = 1
+      -- require("lspconfig").ccls.setup({
+      --   init_options = {
+      --     cache = {
+      --       directory = ".ccls-cache",
+      --     },
+      --   },
+      -- })
+
+      require("nvim-autopairs").setup({})
+
+      vim.diagnostic.config({
+        -- update_in_insert = true,
+        float = {
+          focusable = false,
+          style = "minimal",
+          border = "rounded",
+          source = "always",
+          header = "",
+          prefix = "",
+        },
+      })
+    end,
+  },
+  {
+    "folke/lazydev.nvim",
+    ft = "lua",     -- only load on lua files
+    opts = {
+      library = {
+        -- See the configuration section for more details
+        -- Load luvit types when the `vim.uv` word is found
+        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+      },
     },
+  },
+  {
+    "saghen/blink.cmp",
+    dependencies = {
+      {
+        "L3MON4D3/LuaSnip",
+        -- follow latest release.
+        version = "v2.*",         -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+      },
+    },
+    lazy = false,
+
+    -- use a release tag to download pre-built binaries
+    -- version = "v0.*",
+    build = "rustup run nightly cargo build --release",
+    opts = {
+      keymap = {
+        ["<S-Tab>"] = { "select_next" },
+        ["<Tab>"] = { "select_and_accept", "fallback" },
+        ["<C-j>"] = { "select_next" },
+        ["<C-k>"] = { "select_prev" },
+        ["<C-h>"] = { "show" },
+        ["<C-n>"] = { "show", "select_next" },
+        ["<C-p>"] = { "show", "select_prev" },
+
+        ["<C-space>"] = { "show" },         -- "show_documentiation", "hide_documentation" },
+      },
+
+      appearance = {
+        nerd_font_variant = "normal",
+      },
+      snippets = { preset = "luasnip" },
+      -- snippets = {
+      --   expand = function(snippet)
+      --     require("luasnip").lsp_expand(snippet)
+      --   end,
+      --   active = function(filter)
+      --     if filter and filter.direction then
+      --       return require("luasnip").jumpable(filter.direction)
+      --     end
+      --     return require("luasnip").in_snippet()
+      --   end,
+      --   jump = function(direction)
+      --     require("luasnip").jump(direction)
+      --   end,
+      -- },
+      sources = {
+        default = { "path", "snippets", "lsp", "buffer" },
+        -- optionally disable cmdline completions
+        cmdline = {},
+        providers = {
+          snippets = {
+            name = "LuaSnip",
+            score_offset = 3,
+            module = "blink.cmp.sources.snippets",
+          },
+        },
+      },
+
+      -- experimental signature help support
+      -- signature = { enabled = true },
+
+      -- Custom nvim-cmp like look
+      completion = {
+        menu = {
+          auto_show = true,
+          winblend = vim.o.pumblend,
+          draw = {
+            -- Aligns the keyword you've typed to a component in the menu
+            -- align_to_component = "label", -- or 'none' to disable
+            -- Left and right padding, optionally { left, right } for different padding on each side
+            padding = 1,
+            gap = 1,
+            treesitter = {},             -- "lsp" },
+
+            -- Components to render, grouped by column
+            columns = {
+              { "label", "label_description", gap = 1 },
+              { "kind",  "kind_icon",         "source_name", gap = 1 },
+            },
+            components = {
+              -- detail = {},
+              source_name = {
+                ellipsis = false,
+                width = { fill = true },
+                text = function(ctx)
+                  return "[" .. ctx.source_name .. "]"
+                end,
+              },
+            },
+          },
+        },
+        documentation = {
+          auto_show = true,
+          window = { winblend = vim.o.pumblend },
+        },
+        -- ghost_text = { enabled = true },
+      },
+    },
+  },
 }
