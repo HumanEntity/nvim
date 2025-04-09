73b0006097e3b6bad9423687a38041c5be65efc7
HumanEntity <human_entity@icloud.com>
2025-04-09 16:19:57 +0200

    Some snacks for lunch

---
+++ lua/he/plugins/snacks.lua
@@ -0,0 +1,43 @@
+return {
+  "folke/snacks.nvim",
+  lazy = false,
+  opts = {
+    words = {
+      -- your words configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+    dashboard = {
+      preset = {
+        header = ""
+            .. "██╗  ██╗██╗   ██╗███╗   ███╗ █████╗ ███╗   ██╗  \n"
+            .. "██║  ██║██║   ██║████╗ ████║██╔══██╗████╗  ██║  \n"
+            .. "███████║██║   ██║██╔████╔██║███████║██╔██╗ ██║  \n"
+            .. "██╔══██║██║   ██║██║╚██╔╝██║██╔══██║██║╚██╗██║  \n"
+            .. "██║  ██║╚██████╔╝██║ ╚═╝ ██║██║  ██║██║ ╚████║  \n"
+            .. "╚═╝  ╚═╝ ╚═════╝ ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝  \n"
+            .. "\n"
+            .. "███████╗███╗   ██╗████████╗██╗████████╗██╗   ██╗\n"
+            .. "██╔════╝████╗  ██║╚══██╔══╝██║╚══██╔══╝╚██╗ ██╔╝\n"
+            .. "█████╗  ██╔██╗ ██║   ██║   ██║   ██║    ╚████╔╝ \n"
+            .. "██╔══╝  ██║╚██╗██║   ██║   ██║   ██║     ╚██╔╝  \n"
+            .. "███████╗██║ ╚████║   ██║   ██║   ██║      ██║   \n"
+            .. "╚══════╝╚═╝  ╚═══╝   ╚═╝   ╚═╝   ╚═╝      ╚═╝   \n",
+      },
+      sections = {
+        { section = "header" },
+        {
+          section = "terminal",
+          pane = 2,
+          cmd = "chafa ~/Documents/Wallpapers/fish.jpeg --format symbols --symbols vhalf --size 60x17 --stretch",
+          height = 17,
+          padding = 1,
+        },
+        { section = "keys", gap = 1, padding = 1 },
+        { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+        { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+        { section = "startup" },
+      },
+    },
+  },
+}
