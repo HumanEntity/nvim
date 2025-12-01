43a95122bae2a8fe54ef7190013ea3a7daa156ac
HumanEntity <human_entity@icloud.com>
2025-12-01 20:16:00 +0100

    Added snacks

--- lua/he/plugins/snacks.lua
+++ lua/he/plugins/snacks.lua
@@ -2,42 +2,52 @@ return {
     "folke/snacks.nvim",
     lazy = false,
     opts = {
-        words = {
-            -- your words configuration comes here
-            -- or leave it empty to use the default settings
-            -- refer to the configuration section below
-        },
-        dashboard = {
-            preset = {
-                header = ""
-                    .. "██╗  ██╗██╗   ██╗███╗   ███╗ █████╗ ███╗   ██╗  \n"
-                    .. "██║  ██║██║   ██║████╗ ████║██╔══██╗████╗  ██║  \n"
-                    .. "███████║██║   ██║██╔████╔██║███████║██╔██╗ ██║  \n"
-                    .. "██╔══██║██║   ██║██║╚██╔╝██║██╔══██║██║╚██╗██║  \n"
-                    .. "██║  ██║╚██████╔╝██║ ╚═╝ ██║██║  ██║██║ ╚████║  \n"
-                    .. "╚═╝  ╚═╝ ╚═════╝ ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝  \n"
-                    .. "\n"
-                    .. "███████╗███╗   ██╗████████╗██╗████████╗██╗   ██╗\n"
-                    .. "██╔════╝████╗  ██║╚══██╔══╝██║╚══██╔══╝╚██╗ ██╔╝\n"
-                    .. "█████╗  ██╔██╗ ██║   ██║   ██║   ██║    ╚████╔╝ \n"
-                    .. "██╔══╝  ██║╚██╗██║   ██║   ██║   ██║     ╚██╔╝  \n"
-                    .. "███████╗██║ ╚████║   ██║   ██║   ██║      ██║   \n"
-                    .. "╚══════╝╚═╝  ╚═══╝   ╚═╝   ╚═╝   ╚═╝      ╚═╝   \n",
-            },
-            sections = {
-                { section = "header" },
-                {
-                    section = "terminal",
-                    pane = 2,
-                    cmd = "chafa ~/Documents/Wallpapers/fish.jpeg --format symbols --symbols vhalf --size 60x17 --stretch",
-                    height = 17,
-                    padding = 1,
-                },
-                { section = "keys", gap = 1, padding = 1 },
-                { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
-                { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
-                { section = "startup" },
-            },
-        },
+        picker = {},
+        bigfile = { enabled = true },
+        quickfile = { enabled = true },
+        input = { enabled = true },
+        image = { enabled = true },
     },
+    config = function(_, opts)
+        require("snacks").setup(opts)
+
+        local map = require("he.utils").map
+        local picker = Snacks.picker
+
+        map("n", "<leader>fb", picker.buffers, { desc = "Buffers" })
+        map("n", "<leader>pf", picker.files, { desc = "Find files" })
+        map("n", "<leader>pws", function()
+            local word = vim.fn.expand("<cword>")
+            picker.grep({ cmd = word })
+            -- builtin.grep_string({ search = word })
+        end, { desc = "Live grep" })
+        vim.keymap.set("n", "<leader>pWs", function()
+            local word = vim.fn.expand("<cWORD>")
+            picker.grep({ cmd = word })
+            -- builtin.grep_string({ search = word })
+        end)
+        -- Git
+        vim.keymap.set("n", "<leader>ps", function()
+            local word = vim.fn.input("Grep > ")
+            picker.grep({ args = { word } })
+        end)
+        vim.keymap.set("n", "<leader>vh", picker.help, {})
+
+        map("n", "<C-p>", function()
+            local status = false -- pcall(builtin.git_files)
+
+            if not status then
+                picker.files()
+                -- builtin.find_files()
+            end
+        end, { desc = "Git files" })
+        map("n", "<leader><C-p>", function()
+            vim.cmd("rightbelow vsplit")
+            local status = false -- pcall(builtin.git_files)
+
+            if not status then
+                picker.files()
+            end
+        end, { desc = "Git files" })
+    end,
 }
