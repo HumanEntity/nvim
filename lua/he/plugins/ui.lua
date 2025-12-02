f08268ce57a438b7687736bc17d7e81f5f7d46ba
HumanEntity <human_entity@icloud.com>
2025-12-02 12:11:18 +0100

    Moved twilight to ui.lua

--- lua/he/plugins/ui.lua
+++ lua/he/plugins/ui.lua
@@ -160,4 +160,46 @@ return {
         -- Optional: Lazy load Incline
         event = "VeryLazy",
     },
+    {
+        "folke/twilight.nvim",
+        opts = {
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+        },
+        event = "VeryLazy",
+        config = function()
+            require("twilight").setup({
+                context = 10,
+                treesitter = true,
+                dimming = {
+                    alpha = 0.35,
+                },
+            })
+            -- Dim everything when inserting to force using normal mode
+            -- vim.api.nvim_create_autocmd("InsertEnter", {
+            --     -- command = ":TwilightEnable",
+            --     callback = function()
+            --         require("twilight").setup({
+            --             context = -1,
+            --             treesitter = true,
+            --         })
+            --     end,
+            --     group = "HeGroup",
+            -- })
+            --
+            -- vim.api.nvim_create_autocmd("InsertLeave", {
+            --     -- command = ":TwilightDisable",
+            --     callback = function()
+            --         require("twilight").setup({
+            --             context = 5,
+            --             treesitter = true,
+            --         })
+            --     end,
+            --     group = "HeGroup",
+            -- })
+            --
+            -- vim.cmd("TwilightEnable")
+        end,
+    },
 }
