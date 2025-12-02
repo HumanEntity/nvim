1a9246bac4d64b94d69b332af68f9c234ab86e4a
HumanEntity <human_entity@icloud.com>
2025-12-02 15:31:19 +0100

    Removed todo-comments

--- lua/he/plugins/utils.lua
+++ lua/he/plugins/utils.lua
@@ -12,20 +12,6 @@ return {
         },
     },
     {
-        "folke/todo-comments.nvim",
-        dependencies = { "nvim-lua/plenary.nvim" },
-        event = "VeryLazy",
-        opts = {
-            gui_style = {
-                fg = "NONE", -- The gui style to use for the fg highlight group.
-                bg = "BOLD", -- The gui style to use for the bg highlight group.
-            },
-            -- your configuration comes here
-            -- or leave it empty to use the default settings
-            -- refer to the configuration section below
-        },
-    },
-    {
         "folke/snacks.nvim",
         lazy = false,
         opts = {
