65066a109dc9992632505d0e4b247a3fede5cf1f
HumanEntity <human_entity@icloud.com>
2025-12-02 11:09:21 +0100

    Removed zen-mode as I occasionally use only twilight

--- lua/he/plugins/zen.lua
+++ lua/he/plugins/zen.lua
@@ -1,23 +1,5 @@
 return {
     {
-        "folke/zen-mode.nvim",
-        lazy = false,
-        opts = {
-            window = {
-                backdrop = 1,
-                options = {
-                    number = false, -- disable number column
-                    relativenumber = false,
-                },
-            },
-            -- your configuration comes here
-            -- or leave it empty to use the default settings
-            -- refer to the configuration section below
-        },
-        config = true,
-    },
-    -- Lua
-    {
         "folke/twilight.nvim",
         opts = {
             -- your configuration comes here
